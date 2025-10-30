73aae274bfc41c284ea19f041ea2cea51521a30b
Drew Urenko <drewurenko@gmail.com>
2025-10-30 08:52:09 -0400

    Added functions for singleton class.

--- src/classes/UISDatabase.php
+++ src/classes/UISDatabase.php
@@ -4,11 +4,11 @@ class UISDatabase {
     private static $conn = NULL;
 
     private static function connect() {
-        global $servername, $database, $username, $password;
+        global $servername, $database, $dbUsername, $dbPassword;
 
         if (self::$conn === NULL) {
             try {
-                self::$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+                self::$conn = new PDO("mysql:host=$servername;dbname=$database", $dbUsername, $dbPassword);
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 echo "Connection failed: " . $e->getMessage();
@@ -18,6 +18,44 @@ class UISDatabase {
         }
     }
 
+    public static function getDataFromSQL($sql, $params = null) {
+        self::connect();
+
+        $stmt = self::$conn->prepare($sql);
+        $stmt->execute($params);
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $valuesArray = $stmt->fetchAll();
+        return $valuesArray;
+
+    }
+
+    public static function executeSQL($sql, $params = null, $returnID = false) {
+        self::connect();
+
+        $stmt = self::$conn->prepare($sql);
+        $stmt->execute($params);
+
+        if ($returnID) {
+            return self::$conn->lastInsertId();
+        } else {
+            return true;
+        }
+    }
+
+    public static function startTransaction() {
+        self::connect();
+        self::$conn->beginTransaction();
+    }
+
+    public static function commitTransaction() {
+        self::$conn->commit();
+    }
+
+    public static function rollbackTransaction() {
+        self::$conn->rollback();
+    }
 }
 
 ?>
