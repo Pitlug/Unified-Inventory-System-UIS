e45e2b0f2bcfbab950bd42c807e34ae98cdd186e
Drew Urenko <drewurenko@gmail.com>
2025-11-06 10:44:34 -0500

    Added url check to determine if on pitlug or etowndb.

--- src/web/includes/sitefunctions.php
+++ src/web/includes/sitefunctions.php
@@ -1,22 +1,37 @@
 <?PHP
 function url(){
     global $installDir;
-    if($installDir!=""){
-        $baseFilePath = "/".$installDir;
-    }else{
-        $baseFilePath = "";
+
+    $hostType = getHostType();
+
+    if (!isset($installDir) || $installDir === "") {
+        if ($hostType === "ETOWNDB") {
+            $installDir = "src/web";
+        }
     }
-    if(isset($_SERVER['HTTPS'])){
-        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
-    }
-    else{
-        $protocol = 'http';
-    }
+
+    $baseFilePath = $installDir ? "/" . $installDir : "";
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? "https" : "http";
+
     return $protocol . "://" . $_SERVER['HTTP_HOST'] .  $baseFilePath;
 }
 
 function includeFiles(){
     //TBD
+}
+
+function getHostType() {
+    $host = $_SERVER["HTTP_HOST"];
+
+    if ($host === "uis.pitlug.com") {
+        return "PITLUG";
+    } elseif ($host === "uis.etowndb.com") {
+        return "ETOWNDB";
+    } elseif ($host === "localhost" || $host === "127.0.0.1") {
+        return "LOCAL";
+    } else {
+        return "UNKNOWN";
+    }
 }
 
 ?>
