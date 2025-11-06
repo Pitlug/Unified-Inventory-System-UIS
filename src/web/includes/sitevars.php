722fc588c4071fe5f9c8ce69bfb75479dcc2e56b
Drew Urenko <drewurenko@gmail.com>
2025-11-06 10:44:34 -0500

    Added url check to determine if on pitlug or etowndb.

--- src/web/includes/sitevars.php
+++ src/web/includes/sitevars.php
@@ -1,11 +1,24 @@
 <?php
-global $installDir, $urlForNavBar,$protocol_used,$domain, $data_config_path,$fullInstallPath;
+global $installDir, $urlForNavBar, $protocol_used, $domain, $data_config_path, $fullInstallPath;
 
 $domain = $_SERVER["HTTP_HOST"];
-$protocol_used =isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'?"https://":"http://";
+$protocol_used =isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? "https://" : "http://";
 //$data_config_path = "data_src/api/includes/db_config.php";
 
-    if ($domain == "127.0.0.1" || $domain == "localhost") {
+// Custom logic for production servers
+if ($domain === "uis.pitlug.com") {
+
+    // Root install
+    $installDir = "";
+    $fullInstallPath = "/var/www/uis.pitlug.com/Unified-Inventory-System-UIS";
+
+} elseif ($domain === "uis.etowndb.com") {
+
+    //Subdirectory install
+    $installDir = "src/web";
+    $fullInstallPath = "/var/www/uis.etowndb.com/Unified-Inventory-System-UIS";
+
+} elseif ($domain == "127.0.0.1" || $domain == "localhost") {
 
         $installDir = "Unified-Inventory-System-UIS/src/web";
         if (PHP_OS == "Darwin") {
@@ -22,10 +35,10 @@ $protocol_used =isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'?"https:/
 
         }
 
-    } else {
+} else {
         $installDir = "";
-        $fullInstallPath = "/var/www/uis.pitlug.com/Unified-Inventory-System-UIS";
-    }
+        $fullInstallPath = "/var/www/html";
+}
 
     /*I don't think we are using this.
     if (!is_file($fullInstallPath."/".$data_config_path)) {
@@ -41,14 +54,14 @@ $protocol_used =isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'?"https:/
 include_once "sitefunctions.php";
 
 $url = url();
-$GLOBALS['src'] = dirname(dirname(__DIR__)).'\\';
-$GLOBALS['apiUrl'] = $GLOBALS['src'].'api\\';
+$GLOBALS['src'] = dirname(dirname(__DIR__)) . '\\';
+$GLOBALS['apiUrl'] = $GLOBALS['src'] . 'api\\';
 $GLOBALS['webRoot'] = $url;
-$GLOBALS['cssUrl'] = $url.'/css/';
-$GLOBALS['jsUrl'] = $url.'/js/';
-$GLOBALS['imgUrl'] = $url.'/images/';
-$GLOBALS['classUrl'] = $url.'/classes/';
-$GLOBALS['includeUrl'] = $url.'/includes/';
-$GLOBALS['api-orders'] = $url.'/api/orders/api_orders.php';
-$GLOBALS['inventory'] = $url.'/inventory/';
+$GLOBALS['cssUrl'] = $url . '/css/';
+$GLOBALS['jsUrl'] = $url . '/js/';
+$GLOBALS['imgUrl'] = $url . '/images/';
+$GLOBALS['classUrl'] = $url . '/classes/';
+$GLOBALS['includeUrl'] = $url . '/includes/';
+$GLOBALS['api-orders'] = $url . '/api/orders/api_orders.php';
+$GLOBALS['inventory'] = $url . '/inventory/';
 ?>
