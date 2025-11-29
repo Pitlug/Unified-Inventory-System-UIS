69859737634e8ad6a2202ff8e6615970d28efe9f
Drew Urenko <drewurenko@gmail.com>
2025-11-29 12:51:39 -0500

    Changed my credentials for homelab server.

--- src/classes/env.secure.php
+++ src/classes/env.secure.php
@@ -1,27 +1,26 @@
-
 <?php
 /**
-* src/config/env.local.php
-* Private overrides—NOT tracked in git.
-*/
+ * src/config/env.local.php
+ * Private overrides—NOT tracked in git.
+ */
 return [
-'env' => 'local', // or 'hostinger' or 'homelab' to hard-force
+    'env' => 'local', // or 'hostinger' or 'homelab' to hard-force
 
-'bases' => [
-// override if needed
-],
+    'bases' => [
+        // override if needed
+    ],
 
-'db' => [
-'local' => [
-'pass' => '', // your local DB password if set
-],
-'hostinger' => [
-'pass' => 'gm75fxL#v+6L', // Hostinger DB password
-],
-'homelab' => [
-'host' => '146.135.13.90',
-'user' => 'rein',
-'pass' => 'ChangeMe',
-],
-],
+    'db' => [
+        'local' => [
+            'pass' => '', // your local DB password if set
+        ],
+        'hostinger' => [
+            'pass' => 'gm75fxL#v+6L', // Hostinger DB password
+        ],
+        'homelab' => [
+            'host' => '146.135.13.90',
+            'user' => 'drew',
+            'pass' => 'Yellow5889Bobby',
+        ],
+    ],
 ];
